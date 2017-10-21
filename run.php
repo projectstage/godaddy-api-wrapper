ac0b8043eaef429e52ec21b92549220a9ee6b614
carsten <carsten.lorenz@projectstage.org>
2017-10-21 22:09:54 +0200

    implement test runner

---
+++ run.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carsten
+ * Date: 10/21/17
+ * Time: 10:07 PM
+ */
+require_once 'vendor/autoload.php';
+
+$GoDaddy = new \GoDaddy\GoDaddyClient();
+
+var_dump($GoDaddy);
